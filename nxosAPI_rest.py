529a0d46d60f7dd20c113a83d446a8b680788193
richard palmer <richard.palmer811@gmail.com>
2024-06-18 15:41:02 +0100

    python code for authing against NXOS Rest API and parsing out the token

---
+++ nxosAPI_rest.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from pprint import pprint
+
+url = "https://sbx-nxos-mgmt.cisco.com/api/aaaLogin.json"
+
+payload = json.dumps({
+  "aaaUser": {
+    "attributes": {
+      "name": "admin",
+      "pwd": "Admin_1234!"
+    }
+  }
+})
+
+headers = {
+  'Content-Type': 'application/json'
+  }
+
+username = "admin"
+password = "Admin_1234!"
+
+response = requests.post(url, headers=headers, auth=(username, password),data=payload, verify=False).json()
+
+pprint(response)
+token = response['imdata'][0]['aaaLogin']['attributes']['token']
+print(token)
+#pprint(response.imdata[0].aaaLogin.attributes.token)
